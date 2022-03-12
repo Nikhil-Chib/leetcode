548feda5fc010423a884ee297aec3ca18ebec7d5
Nikhil-Chib <30864530+Nikhil-Chib@users.noreply.github.com>
2022-03-12 12:59:57 +0530

    Time: 2 ms (76.67%), Space: 49.3 MB (38.61%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int jump(int[] nums) {
+        // if(nums.length == 1) return 0;
+        
+        int maxIndexReached = 0;
+        int curr = 0;
+        int nextMaxIndexReachable = 0;
+        int step = 0;
+        
+        while(curr <= maxIndexReached) {
+            if(maxIndexReached >= nums.length-1) break;
+            
+            nextMaxIndexReachable = Math.max(nextMaxIndexReachable, curr+nums[curr]);
+            
+            if(curr == maxIndexReached) {
+                if(nextMaxIndexReachable == maxIndexReached) break;
+                
+                maxIndexReached = nextMaxIndexReachable;
+                step++;
+            }
+            
+            curr++;
+        }
+        
+        if(maxIndexReached >= nums.length-1) return step;
+        return -1;
+    }
+}
