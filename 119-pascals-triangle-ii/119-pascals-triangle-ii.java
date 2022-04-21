b7f723c4611fcbd5d468324b02d129e15d518c76
Nikhil-Chib <30864530+Nikhil-Chib@users.noreply.github.com>
2022-04-21 09:33:00 +0530

    Time: 2 ms (45.54%), Space: 42 MB (36.15%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        return generate(rowIndex+1).get(rowIndex);
+    }
+    
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0; i<numRows; i++) {
+            if(i == 0) {
+                ans.add(Arrays.asList(1));
+                continue;
+            }
+            
+            if(i == 1) {
+                ans.add(Arrays.asList(1,1));
+                continue;
+            }
+            
+            List<Integer> currAns = new ArrayList<>();
+            currAns.add(1);
+            List<Integer> prevAns = ans.get(i-1);
+            for(int j=0; j<prevAns.size()-1; j++) {
+                currAns.add(prevAns.get(j) + prevAns.get(j+1));
+            } 
+            currAns.add(1);
+            
+            ans.add(currAns);
+        }
+        
+        return ans;
+    }
+}
